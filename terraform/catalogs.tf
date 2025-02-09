8bb8acc63e05e9d5f8304eb698b0b126692ad6f1
root <root@DESKTOP-JV1SINT>
2025-02-09 10:39:14 -0300

    Fixed schemas creation in terraform and databricks version

--- terraform/catalogs.tf
+++ terraform/catalogs.tf
@@ -1,9 +1,19 @@
 resource "databricks_catalog" "raw_catalog" {
-    name = "${var.full_name}_raw"
+    name    = "${var.full_name}_raw"
     comment = "Raw data storage"
 }
 
+resource "databricks_schema" "raw_sales" {
+    catalog_name = databricks_catalog.raw_catalog.name
+    name         = "sales"
+}
+
 resource "databricks_catalog" "stg_catalog" {
-    name = "${var.full_name}_stg"
+    name    = "${var.full_name}_stg"
     comment = "Staging data storage"
+}
+
+resource "databricks_schema" "stg_sales" {
+    catalog_name = databricks_catalog.stg_catalog.name
+    name         = "sales"
 }

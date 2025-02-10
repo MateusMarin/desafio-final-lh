fcda25e8ad792556945b63791ccb4b7a3cdfe973
root <root@DESKTOP-JV1SINT>
2025-02-10 17:03:40 -0300

    Ajusted grant permissions on catalogs and schemas

--- terraform/catalogs.tf
+++ terraform/catalogs.tf
@@ -17,3 +17,43 @@ resource "databricks_schema" "stg_sales" {
     catalog_name = databricks_catalog.stg_catalog.name
     name         = "sales"
 }
+
+# Permissões no catálogo RAW
+resource "databricks_grants" "raw_catalog_grants" {
+  catalog = databricks_catalog.raw_catalog.name
+
+  grant {
+    principal  = "users"  # Pode ser um grupo, usuário específico ou 'users' para todos
+    privileges = ["USE_CATALOG", "SELECT", "MODIFY"]
+  }
+}
+
+# Permissões no schema `sales` dentro do RAW
+resource "databricks_grants" "raw_sales_grants" {
+  schema = "${databricks_catalog.raw_catalog.name}.sales"
+
+  grant {
+    principal  = "users"
+    privileges = ["SELECT", "MODIFY"]
+  }
+}
+
+# Permissões no catálogo STG
+resource "databricks_grants" "stg_catalog_grants" {
+  catalog = databricks_catalog.stg_catalog.name
+
+  grant {
+    principal  = "users"
+    privileges = ["USE_CATALOG", "SELECT", "MODIFY"]
+  }
+}
+
+# Permissões no schema `sales` dentro do STG
+resource "databricks_grants" "stg_sales_grants" {
+  schema = "${databricks_catalog.stg_catalog.name}.sales"
+
+  grant {
+    principal  = "users"
+    privileges = ["SELECT", "MODIFY"]
+  }
+}
